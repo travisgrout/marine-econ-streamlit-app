4cc80d334808e7ae1befe714293dc3eb3eee265f
Travis Grout <147847411+travisgrout@users.noreply.github.com>
2025-09-25 15:28:57 -0400

    Adding region definitions back in

--- app.py
+++ app.py
@@ -641,7 +641,7 @@ if plot_mode in estimate_modes:
         with st.expander(expander_title):
             st.divider()
             if plot_mode == "Regional Estimates from Public QCEW Data":
-                st.write("Open ENOW splits coastal states into 8 regions. ")
+                st.write("Open ENOW splits coastal states into 8 regions. The <strong>Great Lakes</strong> region is the coastal counties/zip codes of Minnesota, Michigan, Wisconsin, Illinois, Indiana, Ohio plus Erie County, Pennsylvania and New York counties on the shore of Lake Erie and Lake Ontario. The <strong>Northeast</strong> region is comprised of coastal counties in Maine, New Hampshire, Massachusetts, Rhode Island, and Connecticut. The <strong>Mid-Atlantic</strong> region is comprised of New Jersey, Delaware, Maryland, Virginia, and the Atlantic coasts of Pennsylvania and New York. The <strong>Southeast</strong> region includes coastal counties from North Carolina south to the Florida Keys (Monroe County, Florida). The <strong>Gulf</strong> region includes the west coast of Florida plus coastal counties of Alabama, Mississippi, Louisiana, and Texas. The <strong>West</strong> region is comprised of all coastal counties in California, Oregon, and Washington. Hawaii and coastal Alaska make up the <strong>Pacific</strong> region.")
             elif plot_mode == "State Estimates from Public QCEW Data":
                 if selected_geo == "All Coastal States":
                     st.write("Open ENOW includes all 30 U.S. states with a shoreline on the ocean or the Great Lakes. Within those states, Open ENOW aggregates data for all counties on or near the shoreline. ")
@@ -990,4 +990,5 @@ else: # "Compare to original ENOW"
             st.warning("Not enough overlapping data to calculate statistics.")
     else:
         st.warning("No overlapping data available to compare for the selected filters.")
+
 
